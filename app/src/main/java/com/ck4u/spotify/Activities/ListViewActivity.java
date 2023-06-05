05c86377cf2babb859bc2646415b457e5c75bc77
HieuNguyen <19521507@gm.uit.edu.vn>
2023-06-06 04:30:10 +0700

    update

--- app/src/main/java/com/ck4u/spotify/Activities/ListViewActivity.java
+++ app/src/main/java/com/ck4u/spotify/Activities/ListViewActivity.java
@@ -1,5 +1,7 @@
 package com.ck4u.spotify.Activities;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +11,19 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.OpenableColumns;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.arges.sepan.argmusicplayer.Models.ArgAudio;
+import com.arges.sepan.argmusicplayer.Models.ArgAudioList;
+import com.arges.sepan.argmusicplayer.PlayerViews.ArgPlayerLargeView;
+import com.arges.sepan.argmusicplayer.PlayerViews.ArgPlayerSmallView;
 import com.ck4u.spotify.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -36,8 +44,11 @@ public class ListViewActivity extends AppCompatActivity {
 
     ArrayList<String> arrayListSongName = new ArrayList<>();
     ArrayList<String> arrayListUrl = new ArrayList<>();
+    ArrayList<String> arrayListArtist = new ArrayList<>();
+    ArrayList<Song> songList = new ArrayList<>();
     ArrayAdapter<String> arrayAdapter;
-
+    ArgAudioList playlist= new ArgAudioList(true);
+    ArgPlayerSmallView argMusicPlayer;
     private FirebaseFirestore db= FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +57,17 @@ public class ListViewActivity extends AppCompatActivity {
         listView =findViewById(R.id.recycleview_list);
 
         retrieveSongs();
+        argMusicPlayer = (ArgPlayerSmallView) findViewById(R.id.argmusicplayer);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+
+                Song s = songList.get(position);
+                ArgAudio audio = ArgAudio.createFromURL(s.getSubtitle(), s.getSongName(), s.getSongUrl());
+                argMusicPlayer.play(audio);
+            }
+        });
     }
 
     private void retrieveSongs(){
@@ -57,8 +79,14 @@ public class ListViewActivity extends AppCompatActivity {
                     List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
                     for (DocumentSnapshot d : list) {
                         Song c = d.toObject(Song.class);
+                        Song s = new Song(c.getSongName(),c.getSongUrl(),c.getSubtitle());
+                        songList.add(s);
                         arrayListSongName.add(c.getSongName());
-                        arrayListUrl.add(c.getSongUrl());
+                        arrayListArtist.add(c.getSubtitle());
+                        arrayListUrl.add(c.getSongName());
+                        ArgAudio audio = ArgAudio.createFromURL(c.getSubtitle(), c.getSongName(), c.getSongUrl());
+                        playlist.add(audio);
+                        Log.d(TAG, "DocumentSnapshot data: " + d);
                     }
 
                     arrayAdapter = new ArrayAdapter<String>(ListViewActivity.this,android.R.layout.simple_list_item_1,arrayListSongName){
@@ -72,15 +100,13 @@ public class ListViewActivity extends AppCompatActivity {
                             return view;
                         }
                     };
-
+                    argMusicPlayer.playPlaylist(playlist);
                     listView.setAdapter(arrayAdapter);
                 }
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                // if we do not get any data or any error we are displaying
-                // a toast message that we do not get any data
                 Toast.makeText(ListViewActivity.this, "Fail to get the data.", Toast.LENGTH_SHORT).show();
             }
         });
